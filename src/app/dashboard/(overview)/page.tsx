5de0296b348b0e3561fcbd314fda6c7aa4626e17
smitiwis <sistemas.luismi@gmail.com>
2024-11-03 21:29:46 -0500

    Agregar paginas de usuarios y ventas

--- src/app/dashboard/(overview)/page.tsx
+++ src/app/dashboard/(overview)/page.tsx
@@ -16,17 +16,17 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        {/* <Suspense fallback={<CardsSkeleton />}>
+        <Suspense fallback={<CardsSkeleton />}>
           <CardWrapper />
-        </Suspense> */}
+        </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        {/* <Suspense fallback={<RevenueChartSkeleton />}>
+        <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
         </Suspense>
         <Suspense fallback={<LatestInvoicesSkeleton />}>
           <LatestInvoices />
-        </Suspense> */}
+        </Suspense>
       </div>
     </main>
   );
--- src/ui/dashboard/nav-links.tsx
+++ src/ui/dashboard/nav-links.tsx
@@ -14,11 +14,11 @@ import clsx from 'clsx';
 const links = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
   {
-    name: 'Invoices',
-    href: '/dashboard/invoices',
+    name: 'Usuarios',
+    href: '/dashboard/usuarios',
     icon: DocumentDuplicateIcon,
   },
-  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  { name: 'Ventas', href: '/dashboard/ventas', icon: UserGroupIcon },
 ];
 
 export default function NavLinks() {
